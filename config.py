ce81995d0b0128016cda658c14f7407c0075250a
EsdeathEsdeath <alcatraz.au@gmail.com>
2019-11-27 01:31:38 +0300

    fix db

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql + psycopg2://vlad:1@localhost/test1'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vlad@localhost:5432/test1'
