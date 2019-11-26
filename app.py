b15fa49ccbfe9732c1142782f69242435d52b25e
EsdeathEsdeath <alcatraz.au@gmail.com>
2019-11-27 02:01:12 +0300

    add some post

--- app.py
+++ app.py
@@ -4,11 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 
 
-from posts.blueprint import posts
-
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
 db = SQLAlchemy(app)
 
-app.register_blueprint(posts, url_prefix='/blog')
