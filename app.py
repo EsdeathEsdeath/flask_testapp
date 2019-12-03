cad3481de2635ff31a4b734b0825ae7dfce2a12a
EsdeathEsdeath <alcatraz.au@gmail.com>
2019-12-04 01:02:03 +0300

    migrate

--- app.py
+++ app.py
@@ -3,9 +3,16 @@ from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
 db = SQLAlchemy(app)
 
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
